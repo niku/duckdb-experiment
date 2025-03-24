1569b74aaea7ba5d640896fdbabef1110e971d64
niku <10890+niku@users.noreply.github.com>
2025-03-25 00:13:34 +0900

    Use github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 export default {
+  base: "/duckdb-experiment/",
   build: {
     target: "esnext", // To fix "Top-level await is not available in the configured target environment" error message at `npm run build`
   },
